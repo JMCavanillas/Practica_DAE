43f9d31336bf953597e6d841b73ec4bc3d66ae07
Javier Martínez Cavanillas <javmcav@gmail.com>
2018-09-25 20:47:59 +0200

    Añadido esqueleto inicial del proyecto

---
+++ src/main/java/org/ujaen/practicaDAE/Servidor/ServidorPracDAE.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.ujaen.practicaDAE.Servidor;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author javier
+ */
+@SpringBootApplication
+public class ServidorPracDAE 
+{
+    public static void main(String[] args) throws Exception
+    {
+        
+    }
+}
